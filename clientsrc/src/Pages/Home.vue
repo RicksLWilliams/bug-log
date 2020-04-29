eed96cc56a8e751b8cc10802d29b53358fafcfd6
RicksLWilliams <rlwilliamsix@yahoo.com>
2020-04-29 08:02:05 -0500

    clean up code

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -5,13 +5,8 @@
 
     <div>
       <div class="col-3 m-5 list">
-        <!-- <h1>I am a Bug</h1> -->
         <div class="row">
-          <!-- <h3 class="listTitle pt-1 col-6">{{listData.title}}:</h3> -->
           <div class="col-6 dltList">
-            <!-- <button class="btn pb-0" @click="deleteList(listData.id)">
-              <h4 class="text-danger">X</h4>
-            </button>-->
           </div>
         </div>
         <!-- <add-task v-bind:listData="listData"></add-task> -->
